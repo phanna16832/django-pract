cabf04f3c05e1123b2a422263e56672aff367b0c
phanna16832 <149858943+phanna16832@users.noreply.github.com>
2024-11-15 10:35:20 +0700

    Delete myproject directory

--- myproject/my_app/views.py
+++
@@ -1,8 +0,0 @@
-# views.py
-from django.shortcuts import render,HttpResponse
-
-def home(request):
-    return render(request, 'home.html')
-
-def helloworld(request):
-    return render(request, "helloworld.html")
